7e5272a1709744eeb03f32b861e74093d14e70a8
shuvranshu9 <shuvranshu9@gmail.com>
2026-03-12 15:35:28 +0545

    Number Format Bug fixed

--- FRAGO-FRONTEND/src/components/global/NumberFormat.jsx
+++ FRAGO-FRONTEND/src/components/global/NumberFormat.jsx
@@ -1,8 +1,9 @@
 const NumberFormat = ({ value }) => {
   const formatNumber = (value) => {
     if (value === undefined || value === null) return "";
-    const stringValue = value.toString();
-    return new Intl.NumberFormat("en-IN").format(stringValue.replace(/\D/g, ""));
+    const num = typeof value === "number" ? value : parseFloat(value);
+    if (isNaN(num)) return "";
+    return new Intl.NumberFormat("en-IN").format(Math.round(num));
   };
 
   return <span>{formatNumber(value)}</span>;
